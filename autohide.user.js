e476c22e6780b87dd1b25162d53f7cf732592f99
MisteryMan <MisteryMan@users.noreply.github.com>
2020-06-22 05:14:42 +0200

    Update autohide.user.js
    Version Update

--- autohide.user.js
+++ autohide.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Autohide Missions
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Autohides missions that don't need your attention.
+// @version      1.1
+// @description  Autohides missions that don't need your attention. Added settings and set default settings to do nothing.
 // @author       MisteryKid
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|hatakeskuspeli\.com|missionchief-korea\.com|jocdispecerat112\.com|dispecerske-centrum\.com)\/.*$/
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
