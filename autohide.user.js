edbd2057c5d585c9b42f3d075bfd32351597fec5
MisteryMan <MisteryMan@users.noreply.github.com>
2020-06-23 23:11:48 +0200

    Update autohide.user.js
    Version 1.3:
    Cleaned up the code and streamlined a few things.

--- autohide.user.js
+++ autohide.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Autohide Missions
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Autohides missions that don't need your attention. Added settings and set default settings to do nothing.
 // @author       MisteryKid
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|hatakeskuspeli\.com|missionchief-korea\.com|jocdispecerat112\.com|dispecerske-centrum\.com)\/.*$/
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant        none
 // ==/UserScript==
+
 (function() {
-	'use strict';
-    var frame = document.createElement('div');
+	var frame = document.createElement('div');
     $("#search_input_field_missions").before(frame);
     GM_config.init(
         {
@@ -38,7 +38,10 @@
             {
 
             'open': function() { GM_config.frame.setAttribute("style", "border-radius: 10px; border: 4px solid #000; padding: 20px; height: auto; background: #666;") },
-			'save': function() { location.reload(); }
+			'save': function() {
+                initialize();
+                GM_config.close('autoHideMissions');
+                }
             },
             'frame': frame,
 
@@ -60,130 +63,66 @@
 	// This function is called upon ANY change to the mission list.
     async function mutations(e)
     {
-		//Get the current mission list.
         var Missions = $('.missionSideBarEntry');
-        var greenStatus = GM_config.get('greenStatus');
-        var yellowStatus = GM_config.get('yellowStatus');
-		//Process each mission and change their appearance accordingly
-        for (var i = 0; i < Missions.length; i++)
-        {
-			if (e.id != Missions[i].getAttribute('mission_id')) continue;
-			// Get the mission DIV layout
-			var autoHideChildren = Missions[i].firstElementChild.children;
-            Missions[i].firstElementChild.setAttribute("style", "display: block;");
-            autoHideChildren[1].setAttribute("style", "display: block;");
-			//Process the mission DIV layout and check for red status
-            if (Missions[i].firstElementChild.classList.contains("mission_panel_red"))
-            {
-				//If mission status is red, put it in full display.
-                Missions[i].firstElementChild.setAttribute("style", "display: block;");
-				//also expand the mission so you can actually see what the "problem" is.
-                autoHideChildren[1].setAttribute("style", "display: block;");
-				//Send log entry to browser console.
-                //console.log("RED >> Mission has insufficient Vehicles or no vehicles at all: " + Missions[i].getAttribute('search_attribute'));
-            }
-			// If mission is NOT red, but it IS yellow:
-            else if (Missions[i].firstElementChild.classList.contains("mission_panel_yellow"))
-            {
-                if (yellowStatus == 'Hidden') {
-				//Collapse only, but do not hide yet.
-                    Missions[i].firstElementChild.setAttribute("style", "display: none;");
-                }
-                else if (yellowStatus == 'Collapse') {
-                    //Collapse only, but do not hide yet.
-                    autoHideChildren[1].setAttribute("style", "display: none;");
-                }
-                else { // Default set behavior = Do Nothing
-                    //Collapse only, but do not hide yet.
-                    autoHideChildren[1].setAttribute("style", "display: block;");
-                    Missions[i].firstElementChild.setAttribute("style", "display: none;");
-                }
-				//Send log entry to browser console.
-                //console.log("YELLOW >> Vehicles enroute, hiding panel: " + Missions[i].getAttribute('search_attribute'));
-			}
-			// IF mission is NOT red, and it is NOT yellow, then it must be green.
-            else
-			{
-				//Hide mission, no further interaction is needed.
-                if (greenStatus == 'Hidden') {
-                    Missions[i].firstElementChild.setAttribute("style", "display: none;");
-                }
-                else if (greenStatus == 'Collapse') {
-                    autoHideChildren[1].setAttribute("style", "display: none;");
-                }
-                else { // default behavior is do nothing
-                     autoHideChildren[1].setAttribute("style", "display: block;");
-                    Missions[i].firstElementChild.setAttribute("style", "display: block;");
-                }
-				//Send log entry to browser console.
-                //console.log("GREEN >> Mission has all vehicles, hiding mission from list: " + Missions[i].getAttribute('search_attribute'));
-            }
+        for (var i = 0; i < Missions.length; i++) {
+
+            //console.log(e);
+            stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'))
 
         }
+        //initialize();
     }
+    function stateSwitcher(missionElement, greenStatus, yellowStatus) {
+        // Get mission Element Children, useful for collapsing.
+        var missionElementChildren = missionElement.children;
+        // Reset display setting for both the parent and the child element.
+        missionElement.style.display = "";
+        missionElementChildren[1].style.display = "";
+        // Shorten Classlist variable.
+        var missionStatus = missionElement.classList;
+        if (missionStatus.contains("mission_panel_red"))
+        { // If mission state is Red, remove display style setting
+            state("Nothing", missionElement, missionElementChildren);
+        }
+        else if (missionStatus.contains("mission_panel_yellow"))
+        { // If mission state is YELLOW, pass yellowStatus.setting over to state() function.
+            state(yellowStatus, missionElement, missionElementChildren);
+        }
+        else if (missionStatus.contains("mission_panel_green"))
+        { // If mission state is GREEN, pass greenStatus.setting over to state() function.
+            state(greenStatus, missionElement, missionElementChildren);
+        }
+    };
+    function state(type, item, itemChildren) {
+        switch(type) {
+            case "Nothing":
+                item.style.display = "";
+                itemChildren[1].style.display = "";
+                break;
+            case "Collapse":
+                item.style.display = "";
+                itemChildren[1].style.display = "none";
+                break;
+            case "Hidden":
+                item.style.display = "none";
+                itemChildren[1].style.display = "none";
+                break;
+        };
+    };
 	/*	This function triggers when the page is loaded.
 		All currently active missions will be processed
 		and collapsed or hidden from view, depending on status.
 	*/
-    async function initialize()
+    function initialize()
     {
 		// Get the mission list.
         var Missions = $('.missionSideBarEntry');
-        var greenStatus = GM_config.get('greenStatus');
-        var yellowStatus = GM_config.get('yellowStatus');
 		//Process each mission and change their appearance accordingly
-        for (var i = 0; i < Missions.length; i++)
-        {
-			// Get the mission DIV layout
-			var autoHideChildren = Missions[i].firstElementChild.children;
-            Missions[i].firstElementChild.setAttribute("style", "display: block;");
-            autoHideChildren[1].setAttribute("style", "display: block;");
-			//Process the mission DIV layout and check for red status
-            if (Missions[i].firstElementChild.classList.contains("mission_panel_red"))
-            {
-				//If mission status is red, put it in full display.
-                Missions[i].firstElementChild.setAttribute("style", "display: block;");
-				//also expand the mission so you can actually see what the "problem" is.
-                autoHideChildren[1].setAttribute("style", "display: block;");
-				//Send log entry to browser console.
-                //console.log("RED >> Mission has insufficient Vehicles or no vehicles at all: " + Missions[i].getAttribute('search_attribute'));
-            }
-			// If mission is NOT red, but it IS yellow:
-            else if (Missions[i].firstElementChild.classList.contains("mission_panel_yellow"))
-            {
-                if (yellowStatus == 'Hidden') {
-				//Collapse only, but do not hide yet.
-                    Missions[i].firstElementChild.setAttribute("style", "display: none;");
-                }
-                else if (yellowStatus == 'Collapse') {
-                    //Collapse only, but do not hide yet.
-                    autoHideChildren[1].setAttribute("style", "display: none;");
-                }
-                else { // Default set behavior = Collapse
-                    //Collapse only, but do not hide yet.
-					Missions[i].firstElementChild.setAttribute("style", "display: block;");
-                    autoHideChildren[1].setAttribute("style", "display: block;");
-                }
-				//Send log entry to browser console.
-                //console.log("YELLOW >> Vehicles enroute, hiding panel: " + Missions[i].getAttribute('search_attribute'));
-			}
-			// IF mission is NOT red, and it is NOT yellow, then it must be green.
-            else
-			{
-				//Hide mission, no further interaction is needed.
-                if (greenStatus == 'Hidden') {
-                    Missions[i].firstElementChild.setAttribute("style", "display: none;");
-                }
-                else if (greenStatus == 'Collapse') {
-                    autoHideChildren[1].setAttribute("style", "display: none;");
-                }
-                else { // default behavior is hidden
-                    Missions[i].firstElementChild.setAttribute("style", "display: block;");
-					autoHideChildren[1].setAttribute("style", "display: block;");
-                }
-				//Send log entry to browser console.
-                //console.log("GREEN >> Mission has all vehicles, hiding mission from list: " + Missions[i].getAttribute('search_attribute'));
-            }
+        for (var i = 0; i < Missions.length; i++) {
+
+            //console.log(e);
+            stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'))
+
         }
     }
 })();
