4c404eff7fecc2763147ce859f52aa072faf1803
MisteryMan <MisteryMan@users.noreply.github.com>
2020-06-26 05:02:11 +0200

    Update autohide.user.js
    Fixing conflict with missionOut resulted in the script not working for users that did not use missionOut at all.

--- autohide.user.js
+++ autohide.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autohide Missions
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.5.1
 // @description  Autohides missions that don't need your attention. Added settings and set default settings to do nothing.
 // @author       MisteryKid
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com|hatakeskuspeli\.com|missionchief-korea\.com|jocdispecerat112\.com|dispecerske-centrum\.com)\/.*$/
@@ -104,11 +104,14 @@
         for (var i = 0; i < Missions.length; i++) {
 			var missionID = Missions[i].getAttribute("mission_id");
             var missionOut = JSON.parse(localStorage.getItem("lssm_missionOut"));
-                        if (!missionOut.hasOwnProperty(missionID)) {
-                stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'))
+            if (missionOut == null) {
+                stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'));
             }
-
-        }
+            else {
+                if (missionOut.hasOwnProperty(missionID) !== null) {
+                    stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'));
+                }
+            }        }
     }
     function stateSwitcher(missionElement, greenStatus, yellowStatus) {
         // Get mission Element Children, useful for collapsing.
@@ -159,14 +162,15 @@
         for (var i = 0; i < Missions.length; i++) {
             // Get and convert localStorage.lssm_missionOut to a readable format.
             var missionID = Missions[i].getAttribute("mission_id");
-
             var missionOut = JSON.parse(localStorage.getItem("lssm_missionOut"));
-            if (!missionOut.hasOwnProperty(missionID)) {
-                stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'))
+            if (missionOut == null) {
+                stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'));
             }
-
-
-
+            else {
+                if (missionOut.hasOwnProperty(missionID) !== null) {
+                    stateSwitcher(Missions[i].firstElementChild, GM_config.get('greenStatus'), GM_config.get('yellowStatus'));
+                }
+            }
         }
     }
 })();
